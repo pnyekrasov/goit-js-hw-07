6573d51e4f4da13ce82c9e0573df36573556680b
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-12 23:42:02 +0300

    exercise 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const galleryPicturesEls = galleryItems.map((picture) => {
-    return
-    `
-   img = ${preview} alt = {description}
-   
-`;
+const boxPicturesEl = document.querySelector('.gallery');
+
+const galleryPicturesEls = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="original">
+<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`);
+
+boxPicturesEl.insertAdjacentHTML('beforeEnd', galleryPicturesEls.join(''));
+
+boxPicturesEl.addEventListener('click', handlerOpenOriginal);
+
+function handlerOpenOriginal(e) {e.preventDefault();
+    const instance = basicLightbox.create(e.target.dataset.source);
+instance.show();
 }
-);
-console.log(galleryPicturesEls);
-console.log(galleryItems);
 
