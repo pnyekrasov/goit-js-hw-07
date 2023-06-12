a5f8e8753ee36aed39f0b2f90717c4f6cfcf2403
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-13 00:31:57 +0300

    HW 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const boxPicturesEl = document.querySelector('.gallery');
 
-const galleryPicturesEls = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="original">
+const galleryPicturesEls = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="${original}">
 <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`);
 
 boxPicturesEl.insertAdjacentHTML('beforeEnd', galleryPicturesEls.join(''));
