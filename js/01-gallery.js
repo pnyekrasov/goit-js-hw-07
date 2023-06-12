38f083e406f83f360019c78d03bdaaea0f7b3b00
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-12 18:51:39 +0300

    continuation 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const galleryPicturesEls = galleryItems.map((picture) => {
+    return
+    `
+   img = ${preview} alt = {description}
+   
+`;
+}
+);
+console.log(galleryPicturesEls);
 console.log(galleryItems);
+
