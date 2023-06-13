6962adb89332a86eae1ce94d405fac72a916744d
pnyekrasov <pnekrasov1910@gmail.com>
2023-06-13 16:03:10 +0300

    1 cont

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,12 +5,17 @@ const boxPicturesEl = document.querySelector('.gallery');
 const galleryPicturesEls = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="${original}">
 <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`);
 
-boxPicturesEl.insertAdjacentHTML('beforeEnd', galleryPicturesEls.join(''));
+boxPicturesEl.insertAdjacentHTML('beforeend', galleryPicturesEls.join(''));
 
 boxPicturesEl.addEventListener('click', handlerOpenOriginal);
 
 function handlerOpenOriginal(e) {e.preventDefault();
-    const instance = basicLightbox.create(e.target.dataset.source);
+    const instance = basicLightbox.create(`<img class="gallery__image" src="${e.target.dataset.source}" alt="${e.target.description}"/>`);
 instance.show();
 }
 
+boxPicturesEl.removeEventListener('click', handlerCloseOriginal);
+function handlerCloseOriginal() { };
+// const visible = basicLightbox.visible()
+// instance.close(() => console.log('lightbox not visible anymore'))
+// instance.close()
