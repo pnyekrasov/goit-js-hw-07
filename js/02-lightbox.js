73920003e68ca2ee09c7965cf6d784c8a6ff1c45
pnyekrasov <pnekrasov1910@gmail.com>
2023-07-31 21:18:20 +0300

    correct mistakes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const boxPicturesEl = document.querySelector('.gallery');
+const boxPicturesEl = document.querySelector(".gallery");
 
-const galleryPicturesEls = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+const galleryPicturesEls = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}"/>
   </a>
-</li>`);
+</li>`
+);
 
-boxPicturesEl.insertAdjacentHTML('beforeend', galleryPicturesEls.join(''));
+boxPicturesEl.insertAdjacentHTML("beforeend", galleryPicturesEls.join(""));
 
-const lightboxGalleryEl = new SimpleLightbox('.gallery__item a', {
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 500,
+const lightboxGalleryEl = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
-
-
